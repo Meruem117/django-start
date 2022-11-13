aa58e1013a8c31274c458fcbfcd883d50b4c97aa
Meruem117 <1572677319@qq.com>
2022-11-13 13:24:13 +0800

    change name

--- djangoStart/asgi.py
+++ djangoStart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoStart project.
+ASGI djangoConfig for djangoStart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- djangoStart/wsgi.py
+++ djangoStart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoStart project.
+WSGI djangoConfig for djangoStart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
